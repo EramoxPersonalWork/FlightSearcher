80b7ac55a1ab442b5de33b1aa9069639c8d32880
Franck LENORMAND <franck.lenormand.fr@gmail.com>
2017-04-09 01:40:56 +0200

    Add Currency parsing
    Signed-off-by: Franck LENORMAND <franck.lenormand.fr@gmail.com>

--- find_flight.py
+++ find_flight.py
@@ -18,6 +18,8 @@ from io import StringIO
 import sys
 import getopt
 import argparse
+import json
+import codecs
 
 class SkyscannerAPI(object):
 	'Class to handle requests to Skyscanner REST API'
@@ -32,14 +34,16 @@ class SkyscannerAPI(object):
 #		print("Database version : %s " % data)
 		self.apikey = apiKey
 
-	def __del__(self):
-		#print("Closing DB")
-		#self.db.close()
-		print(__name__ + " destroyed")
+class SkyErrorCode(object):
+	def __init__(self, resp):
+		self.error = resp.status
+		print("code is " + str(self.error))
 
-	def str_resp (self, resp):
-		code = resp.status
-		print("code is " + str(code))
+	def isFailure(self):
+		return self.error != 200
+
+	def __str__ (self):
+		code = self.error
 		txt = "Unknow"
 		if code == 200:
 			txt = "Success"
@@ -59,13 +63,14 @@ class SkyscannerAPI(object):
 			txt = "Server error"
 
 		print("String is " + txt)
-		return str(code) + "(" + resp.reason + ") -> (" + txt + ")";
+		return str(code) + " (" + txt + ")";
 #print_code("test", 200)
 #print_code("test2", 201)
 #print_code("test3", 400)
 #print_code("test4", 403)
 #print_code("test5", 429)
 #print_code("test6", 500)
+
 
 class TravelApiLive(SkyscannerAPI):
 
@@ -166,48 +171,49 @@ class TravelApiLive(SkyscannerAPI):
 			f.write(content)
 
 		return;
+class ServiceParameter(object):
+	def __init__(self, coun, cur, loc, ori, dest, dateOut, dateIn):
+		self.country = coun
+		self.currency = cur
+		self.locale = loc
+		self.originPlace = ori
+		self.destinationPlace = dest
+		self.outboundPartialDate = dateOut
+		self.inboundPartialDate = dateIn
 
+	def getUrl(self):
+		seq = (self.country, self.currency, self.locale, self.originPlace, self.destinationPlace)
+		seq += (self.outboundPartialDate, self.inboundPartialDate)
+		return "/".join(seq)
+
+	def __str__(self):
+		desc = "Parameter for cache service:"
+		return desc
 
 class TravelApiCached(SkyscannerAPI):
 
 	def __init__(self, apiKey):
 		super().__init__(apiKey)
 
-	def buildServiceParameter(self, coun, cur, loc, ori, dest, dateOut, dateIn):
-		serviceParameter = {
-			"country": coun,
-			"currency": cur,
-			"locale": loc,
-			"originPlace": ori,
-			"destinationPlace": dest,
-			"outboundPartialDate": dateOut,
-			"inboundPartialDate": dateIn,
-		}
-#		print("Service parameter crated:" + str(serviceParameter))
-		return serviceParameter
-
-	def _buildUrlFromServiceParam(self, serviceParam):
-		url = serviceParam["country"] + "/"
-		url += serviceParam["currency"] + "/"
-		url += serviceParam["locale"] + "/"
-		url += serviceParam["originPlace"] + "/"
-		url += serviceParam["destinationPlace"] + "/"
-		url += serviceParam["outboundPartialDate"] + "/"
-		url += serviceParam["inboundPartialDate"]
-		return url
 
 	def _browseService(self, service, paramService):
 		header = {
 			"Accept": "application/json",
 		}
 		h = Http()
-		serviceUrl = self._buildUrlFromServiceParam(paramService)
-		dataUrl = self.endpoint + "browse" + service + "/v1.0/" + serviceUrl + "?apikey=" + self.apikey
+		dataUrl = self.endpoint + "browse" + service + "/v1.0/" + paramService.getUrl() + "?apikey=" + self.apikey
 #		print("Poling " + dataUrl)
 		resp, content = h.request(dataUrl, "GET")
-		print("Resp:", self.str_resp(resp))
-		print("Content:\n", content)
-		print("Resp:\n", resp)
+		errorCode = SkyErrorCode(resp)
+		print("Content:\n" + str(content))
+		print("Resp:\n" + str(resp))
+		print("ErrorCode:" + str(errorCode))
+
+		if errorCode.isFailure():
+			return None
+
+		parser = SkyJSONParser(content)
+		parser.getCurrency()
 
 		return content;
 
@@ -223,6 +229,126 @@ class TravelApiCached(SkyscannerAPI):
 	def browseDatesGrid(self, paramService):
 		return self._browseService("grid", paramService)
 
+class SkyJSONParser(object):
+	def __init__(self, content):
+		self.loaded = json.loads(content.decode())
+		print("Content loaded: " + str(self.loaded))
+
+#	def getPlaces(self):
+
+	def getCurrency(self):
+		jsonCur = self.loaded["Currencies"][0]
+		print("Currency: " + str(jsonCur))
+		cur = Currency(jsonCur["Code"],
+				jsonCur["Symbol"],
+				jsonCur["ThousandsSeparator"],
+				jsonCur["DecimalSeparator"],
+				jsonCur["SymbolOnLeft"],
+				jsonCur["SpaceBetweenAmountAndSymbol"],
+				jsonCur["RoundingCoefficient"],
+				jsonCur["DecimalDigits"])
+		print("cur: " + str(cur))
+		return cur
+
+#	def getCarrier(self):
+
+class Place(object):
+	def __init__(self, placeId, iataCode, name, type, skyscannerCode,
+			cityName, cityId, countryName):
+		self.placeId = placeId
+
+class Carrier(object):
+	def __init__(self, carrierId, name):
+		self.carrierId = carrierId
+		self.name = name
+
+class Value(object):
+	def __init__(self, units, decimals):
+		self.units = units
+		self.decimals = decimals
+
+	def printValue(self, currency):
+		return currency.printValue(self)
+
+	def getUnits(self):
+		return self.units
+
+	def getDecimals(self):
+		return self.decimals
+
+	def __str__(self):
+		return str(self.units) + " " + str(self.decimals)
+
+class Currency(object):
+	def __init__(self, code, symbol,
+			thousandsSeparator, decimalSeparator,
+			symbolOnLeft, spaceBetweenAmountAndSymbol,
+			roundingCoefficient, decimalDigits):
+		self.code = code
+		self.symbol = symbol
+		self.thousandsSeparator = thousandsSeparator
+		self.decimalSeparator = decimalSeparator
+		self.symbolOnLeft = symbolOnLeft
+		self.spaceBetweenAmountAndSymbol = spaceBetweenAmountAndSymbol
+		self.roundingCoefficient = roundingCoefficient
+		self.decimalDigits = decimalDigits
+
+	def __str__(self):
+		tempV = Value("0123456789", "ABCDEF")
+		print("tempC: " + str(tempV))
+		return self.code + ": " + self.printValue(tempV)
+
+	def printValue(self, value):
+		line = ""
+		if self.symbolOnLeft:
+			line += self.symbol
+			if self.spaceBetweenAmountAndSymbol:
+				line += " "
+
+		strUnits = str(value.getUnits())
+		unitDigits = 3
+
+		print("strU: " + strUnits)
+
+		start = 0
+		end = len(strUnits) % unitDigits
+		line += strUnits[start : end]
+		while end < len(strUnits):
+			if end != 0:
+				line += self.decimalSeparator
+			start = end
+			end += unitDigits
+			line += strUnits[start : end]
+
+		print("formatees: " + line)
+
+		line += self.decimalSeparator
+
+		line += str(value.getDecimals())[0 : self.decimalDigits]
+
+		if not self.symbolOnLeft:
+			line += self.symbol
+			if self.spaceBetweenAmountAndSymbol:
+				line += " "
+
+		return line
+
+	def getValue(self, string):
+		elm = string.split(self.decimalSeparator)
+		return Value(elm[0],elm[1])
+
+
+class SkyContent(object):
+	def __init__(self, content):
+		self.content = content
+
+#	def process(self):
+
+#	def processCurrency(self):
+
+#	def processCarriers(self):
+
+#	def processPlaces(self):
 
 parser = argparse.ArgumentParser(description='Find a flight sing Skyscanner API')
 parser.add_argument('--key', help='Skyscanner API key', required=True)
@@ -236,7 +362,8 @@ def main(argv=None):
 #	tal = TravelApiLive()
 #	tal.create_session()
 	tac = TravelApiCached(args["key"])
-	param = tac.buildServiceParameter("UK", "GBP", "en-GB", "EDI", "LHR", "2017-05-30", "2017-06-02")
+	param = ServiceParameter("UK", "GBP", "en-GB", "EDI", "LHR", "2017-05-30", "2017-06-02")
+	print("param: " + str(param) + param.getUrl())
 #	print("param: " + str(param))
 #	print("browsing quotes")
 #	tac.browseQuotes(param)
@@ -244,9 +371,8 @@ def main(argv=None):
 #	tac.browseRoutes(param)
 #	print("browsing dates")
 #	tac.browseDates(param)
-#	print("browsing dates as grid")
+	print("browsing dates as grid")
 	res = tac.browseDatesGrid(param)
-	print("res: " + res)
 
 	return 0
 
